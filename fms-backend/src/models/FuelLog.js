21619036c4f8a90ef7311f2b7ab2be3b8c694caa
Sinchana91123 <sinchanar.1683@gmail.com>
2026-06-20 15:26:15 +0530

    Uncomment FuelLog model definition
    Uncommented the FuelLog model definition and its exports.

--- fms-backend/src/models/FuelLog.js
+++ fms-backend/src/models/FuelLog.js
@@ -1,3 +1,20 @@
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database');
+
+// const FuelLog = sequelize.define('FuelLog', {
+//   id:             { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+//   vehicle_id:     { type: DataTypes.INTEGER, allowNull: false },
+//   driver_id:      { type: DataTypes.INTEGER },
+//   litres:         { type: DataTypes.FLOAT, allowNull: false },
+//   cost_per_litre: { type: DataTypes.DECIMAL(8,2) },
+//   total_cost:     { type: DataTypes.DECIMAL(10,2) },
+//   odometer_km:    { type: DataTypes.FLOAT },
+//   fuel_type:      { type: DataTypes.STRING(20) },
+//   station_name:   { type: DataTypes.STRING(100) },
+//   filled_at:      { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+// }, { tableName: 'fuel_logs', timestamps: true });
+
+// module.exports = FuelLog;
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
