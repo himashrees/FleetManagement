95520b9bd4b7b800c1ac21cbb6a68fda1f0a0768
Sinchana91123 <sinchanar.1683@gmail.com>
2026-06-20 15:25:10 +0530

    Update fuel.routes.js

--- fms-backend/src/routes/fuel.routes.js
+++ fms-backend/src/routes/fuel.routes.js
@@ -1,3 +1,14 @@
+// const router = require('express').Router();
+// const ctrl = require('../controllers/fuel.controller');
+// const { isAuthenticated } = require('../middlewares/auth.middleware');
+// const { isManager, scopeDriverTo } = require('../middlewares/role.middleware');
+
+// router.get('/',                  isAuthenticated, scopeDriverTo('driver_id'), ctrl.getAll);
+// router.post('/',                 isAuthenticated, isManager,                  ctrl.create);
+// router.get('/stats/:vehicle_id', isAuthenticated,                             ctrl.getStats);
+// router.delete('/:id',            isAuthenticated, isManager,                  ctrl.remove);
+
+// module.exports = router;
 const router = require('express').Router();
 const ctrl = require('../controllers/fuel.controller');
 const { isAuthenticated } = require('../middlewares/auth.middleware');
