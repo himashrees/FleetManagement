23417c971ffd94a654e490046a6f92a44271a0e8
Sinchana91123 <sinchanar.1683@gmail.com>
2026-06-20 15:27:06 +0530

    Enable isAuthenticated middleware
    Uncomment the isAuthenticated middleware function.

--- fms-backend/src/middlewares/auth.middleware.js
+++ fms-backend/src/middlewares/auth.middleware.js
@@ -1,3 +1,8 @@
+// const isAuthenticated = (req, res, next) => {
+//   if (req.session && req.session.user) return next();
+//   return res.status(401).json({ success: false, message: 'Please login to continue' });
+// };
+// module.exports = { isAuthenticated };
 const isAuthenticated = (req, res, next) => {
   if (req.session && req.session.user) return next();
   return res.status(401).json({ success: false, message: 'Please login to continue' });
